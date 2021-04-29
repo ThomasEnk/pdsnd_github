4f807cc99ffc71ad45e73830630b33dc1334c8d3
Thomas Enk <thomas.enk@gmail.com>
2021-04-29 20:03:30 +0200

    Rename raw_data() with display_raw_data() in bikeshare_2v5.py

--- bikeshare_2v5.py
+++ bikeshare_2v5.py
@@ -190,7 +190,7 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-def raw_data(df):
+def display_raw_data(df):
     """Displays raw datas."""
 
     #Ask if you want raw data to be displayed
@@ -223,7 +223,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        raw_data(df)
+        display_raw_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
